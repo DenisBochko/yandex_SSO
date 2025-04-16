57cf1da82adcba827ef69a188c2295017f7fe9e6
reElcy <reElcy@yandex.ru>
2025-04-16 18:20:49 +0300

    Added: retry sending to kafka

--- pkg/kafka/kafka.go
+++ pkg/kafka/kafka.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"context"
+	"time"
 
 	"github.com/IBM/sarama"
 	"go.uber.org/zap"
@@ -21,7 +22,8 @@ func NewSyncProducer(ctx context.Context, log *zap.Logger, cfg KafkaConfig) (sar
 
 	config := sarama.NewConfig()
 	config.Producer.Partitioner = sarama.NewRandomPartitioner // распределяем сообщения по партициям случайным образом
-	// config.Producer.Partitioner = sarama.NewRoundRobinPartitioner // распределяем сообщения по партициям по кругу
+	config.Producer.Retry.Max = 5                             // максимальное количество попыток отправки сообщения
+	config.Producer.Retry.Backoff = 100 * time.Millisecond    // время ожидания между попытками
 	config.Producer.Return.Successes = true
 	config.Producer.RequiredAcks = sarama.WaitForAll // ждём подтверждения от всех брокеров
 	producer, err := sarama.NewSyncProducer(brokers, config)
@@ -30,7 +32,6 @@ func NewSyncProducer(ctx context.Context, log *zap.Logger, cfg KafkaConfig) (sar
 		log.Error("failed to create kafka producer", zap.Error(err))
 		return nil, err
 	}
-	
 
 	log.Info("Kafka producer created", zap.Strings("brokers", brokers), zap.String("topic", cfg.Topic))
 	return producer, err
