1ccdd361406fa21e6fee59fd62fc81bccbbf5cdd
reElcy <reElcy@yandex.ru>
2025-04-16 23:29:07 +0300

    Updated: creation of producer

--- pkg/kafka/kafka.go
+++ pkg/kafka/kafka.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/IBM/sarama"
@@ -20,21 +21,56 @@ func NewSyncProducer(ctx context.Context, log *zap.Logger, cfg KafkaConfig) (sar
 		return nil, sarama.ErrBrokerNotFound
 	}
 
+	// Конфигурация Kafka
 	config := sarama.NewConfig()
-	config.Producer.Partitioner = sarama.NewRandomPartitioner // распределяем сообщения по партициям случайным образом
-	config.Producer.Retry.Max = 5                             // максимальное количество попыток отправки сообщения
-	config.Producer.Retry.Backoff = 100 * time.Millisecond    // время ожидания между попытками
+	config.Version = sarama.V2_1_0_0 // или та версия, которая используется в твоём Kafka
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Retry.Max = 5
+	config.Producer.Retry.Backoff = 100 * time.Millisecond
 	config.Producer.Return.Successes = true
-	config.Producer.RequiredAcks = sarama.WaitForAll // ждём подтверждения от всех брокеров
-	producer, err := sarama.NewSyncProducer(brokers, config)
+	config.Producer.RequiredAcks = sarama.WaitForAll
 
+	// Создаём топик, если его нет
+	if err := createTopic(cfg, config, log); err != nil {
+		log.Error("failed to create topic", zap.Error(err))
+		return nil, err
+	}
+
+	// Создание продюсера
+	producer, err := sarama.NewSyncProducer(brokers, config)
 	if err != nil {
 		log.Error("failed to create kafka producer", zap.Error(err))
 		return nil, err
 	}
 
 	log.Info("Kafka producer created", zap.Strings("brokers", brokers), zap.String("topic", cfg.Topic))
-	return producer, err
+	return producer, nil
+}
+
+// createTopic создаёт топик, если он не существует
+func createTopic(cfg KafkaConfig, config *sarama.Config, log *zap.Logger) error {
+	admin, err := sarama.NewClusterAdmin(cfg.Brokers, config)
+	if err != nil {
+		return err
+	}
+	defer admin.Close()
+
+	topicDetail := &sarama.TopicDetail{
+		NumPartitions:     5,
+		ReplicationFactor: 3,
+	}
+
+	err = admin.CreateTopic(cfg.Topic, topicDetail, false)
+	if err != nil {
+		if errors.Is(err, sarama.ErrTopicAlreadyExists) {
+			log.Info("topic already exists", zap.String("topic", cfg.Topic))
+			return nil
+		}
+		return err
+	}
+
+	log.Info("topic created", zap.String("topic", cfg.Topic))
+	return nil
 }
 
 func NewAsyncProducer(ctx context.Context, cfg KafkaConfig) (sarama.AsyncProducer, error) {
